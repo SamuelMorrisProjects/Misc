e47f2dc786c18c85c9bdca644c389399f5b91203
Samuel Morris <149737652+SamuelMorrisProjects@users.noreply.github.com>
2023-11-03 12:35:01 -0400

    Update Wizard'sGrabBag.py
    Added a Help command. Displays all commands accessible via the CLI.

--- Wizard'sGrabBag.py
+++ Wizard'sGrabBag.py
@@ -1,7 +1,6 @@
-
 WizardsBag = ["Dragon Scale Wand", "Enchanted Grimoire","Eye of Newt",] 
-def Show_Menu():
-     return("COMMAND MENU"+"\n"+"show- Shows all items"+"\n"+"add- Add an Item"+"\n"+"drop - Drop a Item"+"\n"+"edit- Edit an Item"+"\n"+"exit- exit the program")
+def Show_Menu(param):
+     return("\n""COMMAND MENU"+"\n"+"show- Shows all items"+"\n"+"add- Add an Item"+"\n"+"drop - Drop a Item"+"\n"+"edit- Edit an Item"+"\n"+"help- Displays Commands"+"\n"+"exit- exit the program"+"\n")
 
 def If_In_List(edit, list1): 
     if edit in range(1, (len(list1))+1): 
@@ -43,9 +42,9 @@ def Exit_Program(Param):
     print("Bye!")
     quit()
 
-UserCommands = {"edit":Edit_Item, "add":Add_Item, "show":Show_Items, "drop":Drop_Item, "exit":Exit_Program }
+UserCommands = {"edit":Edit_Item, "add":Add_Item, "show":Show_Items, "drop":Drop_Item, "exit":Exit_Program, "help":Show_Menu }
 def main():
-    print("The Wizards Inventory Program"+"\n"+"\n"+Show_Menu()+"\n") 
+    print("The Wizards Inventory Program"+"\n"+"\n"+Show_Menu(WizardsBag)+"\n") 
     while True:
         Command = input("Command: ").rstrip()
         if Command not in UserCommands:
