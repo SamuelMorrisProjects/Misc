0d1a742deb12ac760e9188c1451062ee8e2a12a7
Samuel Morris <149737652+SamuelMorrisProjects@users.noreply.github.com>
2023-12-20 14:39:40 -0500

    Add files via upload

---
+++ GuessingGame.cpp
@@ -0,0 +1,21 @@
+#include <random>
+#include <iostream> 
+using namespace std; 
+  
+// Driver Code 
+int main() 
+{ 
+    int guess;
+    random_device dev;
+    mt19937 rng(dev());
+    uniform_int_distribution<std::mt19937::result_type> dist6(1,10); 
+    cout<<"Guess a number between one and ten: ";
+    cin>>guess; 
+    int rand_num;
+    rand_num = dist6(rng);
+    while (guess!=rand_num){
+        cout<<"You got it wrong Guess again!\nGuess a number between one and ten: "; cin>>guess;
+    }
+    cout<<"You got it!!";
+    return 0;
+}
---
+++ GuessingGame.py
@@ -0,0 +1,7 @@
+from random import randint
+guess=int(input("Guess the number I am thinking of its somewhere btween 1-10: "))
+x = randint(1,10)
+while guess != x:
+    print("Try again\n")
+    guess=int(input("Guess the number I am thinking of its somewhere btween 1-10: "))
+print('You got it')
