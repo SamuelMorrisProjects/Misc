d87bcdb42027d5873ba6b48c594c819a908375b9
Samuel Morris <149737652+SamuelMorrisProjects@users.noreply.github.com>
2024-02-05 14:03:46 -0500

    Added length checker
    Outputs the length of a string or integer in rust

--- main.rs
+++ main.rs
@@ -1,37 +1,14 @@
+
 use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
-
-fn main() 
-{ 
-    println!("Guess a number between one and ten\nPlease input your guess");
-    let rand_num = rand::thread_rng().gen_range(1..=100);
-    let mut guesses:i32 = 0;
-    loop
-    {
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!");
-        match guess.cmp(&rand_num)
-        {
-            Ordering::Less => 
-            {
-                println!("Too small!");
-            }
-
-            Ordering::Greater => 
-            {
-                println!("Too big!");
-            }
-
-            Ordering::Equal =>
-            {
-                println!("You win!, you got in {guesses} guesses");
-                break;
-            }
-        }
-        guesses = guesses+1
-    }
+use unicode_segmentation::UnicodeSegmentation;
+fn main() {
+    println!("Enter a string or char that you want to get the length of: ");
+    let mut userinput: String = String::new();
+    io::stdin()
+        .read_line(&mut userinput)
+        .expect("Failed to read input");
+    println!("\nThe length of {} is {} (Including whitespaces) ",userinput, length(&userinput));
+}
+fn length(string:&String) -> usize {
+    return string.graphemes(true).count()-1 
 }
